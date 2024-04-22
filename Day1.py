3596f5512c9e3ec6b7f85373d0ba05f9e767b19a
Supachot <Supachot@welovebug.biz>
2024-04-22 11:17:23 +0700

    [Updated]: เพิ่มตัวอย่างตัวแปร

--- Day1.py
+++ Day1.py
@@ -5,8 +5,9 @@ print("*" * 10)
 
 # Variable
 pet = ["dog", "cat"]
-print(pet)
-
+rated = 4.5
 values = 10
+print(pet)
+print(rated)
 print(values) # ปริ้นค่าใน Variable
 print("The value is: " + str(values)) # แสดงผลลัพธ์เป็นตัวหนังสือ พร้อมค่าจากใน Variable
