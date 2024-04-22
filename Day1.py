a1ef3061232749649e759b7f3668a6ec5d8689ad
Supachot <Supachot@welovebug.biz>
2024-04-22 10:59:21 +0700

    [Updated]: การแสดงผลข้อมูล และใส่ตัวแปร

---
+++ Day1.py
@@ -0,0 +1,12 @@
+# แสดงผลข้อมูล
+# print("hello world")
+# print("*" * 10)
+
+
+# Variable
+pet = ["dog", "cat"]
+print(pet)
+
+values = 10
+print(values) # ปริ้นค่าใน Variable
+print("The value is: " + str(values)) # แสดงผลลัพธ์เป็นตัวหนังสือ พร้อมค่าจากใน Variable
