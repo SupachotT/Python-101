283f6f0da4597038b74682910e86ad9601da0eb6
Supachot <Supachot@welovebug.biz>
2024-04-22 11:18:57 +0700

    [Updated]: เพิ่มตัวอย่างตัวแปร

--- Day1.py
+++ Day1.py
@@ -7,7 +7,9 @@ print("*" * 10)
 pet = ["dog", "cat"]
 rated = 4.5
 values = 10
+isPublished = True
 print(pet)
 print(rated)
 print(values) # ปริ้นค่าใน Variable
 print("The value is: " + str(values)) # แสดงผลลัพธ์เป็นตัวหนังสือ พร้อมค่าจากใน Variable
+print(isPublished)
