5a97ac28f151e78fce6e3323fa056637a7b339a8
Supachot <Supachot@welovebug.biz>
2024-04-23 11:54:27 +0700

    [Updated]: If statements

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -29,3 +29,31 @@ values1 = 3.6
 values2 = -23.6
 print(round(values1)) # การปัดทศนิยม
 print(abs(values2)) # จะได้เป็นเลขตัวนั้นที่เป็นค่าบวกเท่านั้น
+
+
+# ---------- If Statements ----------
+"""
+Conditions
+
+if it is hot
+    It is a hot day
+    Drink plenty of water
+otherwise if it is cold
+    It is a cold day
+    Wear warm clothes
+otherwise
+    It is a lovely day
+
+
+"""
+
+is_hot = False
+is_cold = True
+if is_hot:
+    print("It is a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It is a cold day")
+    print("Wear warm clothes")
+else:
+    print("It is a lovely day")
