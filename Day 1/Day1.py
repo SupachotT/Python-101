30051baa40388f1b4845f1c3b259d2418ac64c34
Supachot <Supachot@welovebug.biz>
2024-04-23 09:24:42 +0700

    [Added]: เพิ่ม folder และปรับการวาง Code

---
+++ Day 1/Day1.py
@@ -0,0 +1,47 @@
+# ---------- แสดงผลข้อมูล ----------
+print("hello world")
+print("*" * 10)
+
+
+# ---------- Variable ----------
+pet = ["dog", "cat"]
+rated = 4.5
+values = 10
+isPublished = True
+print(pet)
+print(rated)
+print(values) # ปริ้นค่าใน Variable
+print("The value is: " + str(values)) # แสดงผลลัพธ์เป็นตัวหนังสือ พร้อมค่าจากใน Variable
+print(isPublished)
+
+
+# ---------- Receiving Input ----------
+name = input('What is your name ? ')
+favoriteColor = input('What is your favorite color ? ')
+print(name + " like " + favoriteColor + ".")
+
+
+# ---------- Typing Conversion ----------
+birthYear = input('Brith year ? ')
+age = 2024 - int(birthYear)
+print(type(age))
+print(age)
+
+weight_kg = input('Weight (Kg): ')
+weight_lbs = int(weight_kg) / 0.45
+print(weight_lbs)
+print("Weight(lbs) is: " + str(weight_lbs) + " lbs.")
+
+
+# ---------- String ----------
+course = 'Python of Beginner'
+courseDisc = '''
+Hi Tar,
+
+Here is our first email to you
+
+Thank you,
+The support team.
+'''
+print(course)
+print(courseDisc)
